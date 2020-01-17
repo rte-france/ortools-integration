68f5c52f1253827479e510194f3a6ad064622cbe
Eric D <eric.dumont@eurodecision.com>
2020-01-17 15:58:18 +0100

    update tests c++

--- tests/cpp/testAlwaysFail/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "ortools/linear_solver/linear_solver.h"
-
-int main(int argc, char** argv){
-	std::cout << "testXpress1" << std::endl;
-	return -1;
-}
---
+++ tests/cpp/testXpressMultipleInstances/main.cpp
@@ -0,0 +1,47 @@
+#include "ortools/linear_solver/linear_solver.h"
+
+using operations_research::MPSolver;
+using operations_research::MPObjective;
+
+int main(int argc, char** argv){
+	double objValue_l = 0.;
+	{
+		MPSolver * solver = new MPSolver("simple_lp_program", MPSolver::XPRESS_LINEAR_PROGRAMMING);
+		VLOG(1) << "testVlog" << std::endl;
+		auto x = solver->MakeNumVar(0., 5., "testVar");
+		
+		MPObjective* const objective = solver->MutableObjective();
+		objective->SetCoefficient(x, 1.);
+		objective->SetMaximization();
+		solver->set_time_limit(1000.);
+		
+		solver->Solve();
+		
+		objValue_l += solver->Objective().Value();
+
+		std::cout << "Objective = " << solver->Objective().Value() << std::endl;
+		
+		{
+			MPSolver * solver = new MPSolver("simple_lp_program", MPSolver::XPRESS_LINEAR_PROGRAMMING);
+			VLOG(1) << "testVlog" << std::endl;
+			auto x = solver->MakeNumVar(0., 5., "testVar");
+			
+			MPObjective* const objective = solver->MutableObjective();
+			objective->SetCoefficient(x, 1.);
+			objective->SetMaximization();
+			solver->set_time_limit(1000.);
+			
+			solver->Solve();
+			
+			objValue_l += solver->Objective().Value();
+
+			std::cout << "Objective = " << solver->Objective().Value() << std::endl;
+		
+			delete solver;
+		}
+	
+		delete solver;
+	}
+	
+	return (objValue_l != 10.);
+}
